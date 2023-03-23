36a40dba7d5a550d0647b0103cb28079f0b1ae74
DALM1 <92697916+DALM1@users.noreply.github.com>
2023-03-24 00:55:04 +0100

    Updates

--- src/server/server.rs
+++ src/server/server.rs
@@ -13,36 +13,36 @@ fn main() {
 
                 let mut buffer = [0; 1024];
 
-                // Envoi du message de demande de mot de passe
+               
                 let message = "Entrez le mot de passe : ";
                 stream.write(message.as_bytes()).unwrap();
 
-                // Lecture du mot de passe envoyé par le client
+               
                 stream.read(&mut buffer).unwrap();
                 let password = String::from_utf8_lossy(&buffer[..]).trim().to_string();
 
-                // Vérification du mot de passe
+               
                 if password != "my_secret_password" {
                     let message = "Mot de passe incorrect";
                     stream.write(message.as_bytes()).unwrap();
                     continue;
                 }
 
-                // Envoi du message de demande de nom d'utilisateur
+               
                 let message = "Entrez votre nom d'utilisateur : ";
                 stream.write(message.as_bytes()).unwrap();
 
-                // Lecture du nom d'utilisateur envoyé par le client
+                
                 stream.read(&mut buffer).unwrap();
                 let username = String::from_utf8_lossy(&buffer[..]).trim().to_string();
                 println!("L'utilisateur {} est connecté", username);
 
                 loop {
-                    // Envoi du message de demande de commande
+                   
                     let message = "> ";
                     stream.write(message.as_bytes()).unwrap();
 
-                    // Lecture de la commande envoyée par le client
+                    
                     let mut buffer = [0; 1024];
                     stream.read(&mut buffer).unwrap();
                     let command = String::from_utf8_lossy(&buffer[..]).trim().to_string();
@@ -51,13 +51,13 @@ fn main() {
                         break;
                     }
 
-                    // Traitement de la commande
+                   
                     let response = match command.as_str() {
                         "/help" => "Liste des commandes : /help, /quit",
                         _ => "Commande inconnue",
                     };
 
-                    // Envoi de la réponse
+                   
                     stream.write(response.as_bytes()).unwrap();
                 }
             }
