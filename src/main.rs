b33d40990b2d778aa40f4c693ee7c0e0ef664ba7
DALM1 <92697916+DALM1@users.noreply.github.com>
2023-03-23 21:35:02 +0100

    Updates

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::io::{self, prelude::*, BufReader};
-use std::net::{TcpStream};
+use std::net::TcpStream;
 use std::str;
 
 fn main() -> std::io::Result<()> {
@@ -9,13 +9,19 @@ fn main() -> std::io::Result<()> {
     io::stdin().read_line(&mut password)?;
     write!(stream, "{}", password)?;
     let mut reader = BufReader::new(&stream);
-    loop {
-        let mut message = String::new();
-        io::stdin().read_line(&mut message)?;
-        write!(stream, "{}", message)?;
-        let mut buffer = [0; 1024];
-        reader.read(&mut buffer)?;
-        print!("{}", str::from_utf8(&buffer).expect("Invalid UTF-8"));
+    let mut buffer = String::new();
+    reader.read_line(&mut buffer)?;
+    if buffer.trim() == "password accepted" {
+        println!("Le mot de passe est accepté!");
+        loop {
+            let mut message = String::new();
+            io::stdin().read_line(&mut message)?;
+            write!(stream, "{}", message)?;
+            let mut buffer = String::new();
+            reader.read_line(&mut buffer)?;
+            print!("{}", buffer);
+        }
+    } else {
+        println!("Le mot de passe est incorrect!");
     }
-    Ok(())
 }
