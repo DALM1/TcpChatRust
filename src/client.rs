b253c986b95ef91bede6f5c592c161222a4aa378
DALM1 <92697916+DALM1@users.noreply.github.com>
2023-03-24 00:52:33 +0100

    Updates

--- src/client.rs
+++ src/client.rs
@@ -6,7 +6,7 @@ fn main() {
     let mut stream = TcpStream::connect("127.0.0.1:5000").unwrap();
     let mut buffer = [0; 1024];
 
-    // Lire la réponse du serveur après la connexion
+   
     stream.read(&mut buffer).unwrap();
     let response = String::from_utf8_lossy(&buffer[..]).trim().to_string();
     println!("{}", response);
@@ -18,7 +18,7 @@ fn main() {
 
     stream.write(password.trim().as_bytes()).unwrap();
 
-    // Utiliser un thread pour lire les réponses du serveur
+   
     let read_stream = stream.try_clone().unwrap();
     let read_handle = thread::spawn(move || {
         let mut buffer = [0; 1024];
@@ -58,6 +58,6 @@ fn main() {
         }
     }
 
-    // Attendre que le thread de lecture se termine avant de fermer la connexion
+   
     read_handle.join().unwrap();
 }
