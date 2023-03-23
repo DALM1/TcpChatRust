90907a89150ad28e17f25635f9b8247bceb9b077
DALM1 <92697916+DALM1@users.noreply.github.com>
2023-03-23 21:35:27 +0100

    Updates

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::io::{self, prelude::*, BufReader};
-use std::net::TcpStream;
+use std::net::{TcpStream};
 use std::str;
 
 fn main() -> std::io::Result<()> {
@@ -17,11 +17,19 @@ fn main() -> std::io::Result<()> {
             let mut message = String::new();
             io::stdin().read_line(&mut message)?;
             write!(stream, "{}", message)?;
-            let mut buffer = String::new();
-            reader.read_line(&mut buffer)?;
-            print!("{}", buffer);
+            let mut buffer = [0; 1024];
+            let n = reader.read(&mut buffer)?;
+            if n == 0 {
+                println!("Déconnecté du serveur");
+                break;
+            }
+            print!("{}", str::from_utf8(&buffer[..n]).expect("Invalid UTF-8"));
+            if message.trim() == "exit" {
+                break;
+            }
         }
     } else {
         println!("Le mot de passe est incorrect!");
     }
+    Ok(())
 }
