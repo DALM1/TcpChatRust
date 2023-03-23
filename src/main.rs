ad1d8b1dc8660a79a4d74696161057250fdd5a4b
DALM1 <92697916+DALM1@users.noreply.github.com>
2023-03-23 21:31:11 +0100

    Updates

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,13 @@ fn main() -> std::io::Result<()> {
     io::stdin().read_line(&mut password)?;
     write!(stream, "{}", password)?;
     let mut reader = BufReader::new(&stream);
-    let mut buffer = String::new();
-    reader.read_line(&mut buffer)?;
-    if buffer.trim() == "password accepted" {
-        println!("Le mot de passe est accepté!");
-        loop {
-            let mut message = String::new();
-            io::stdin().read_line(&mut message)?;
-            write!(stream, "{}", message)?;
-            let mut buffer = [0; 1024];
-            reader.read(&mut buffer)?;
-            print!("{}", str::from_utf8(&buffer).expect("Invalid UTF-8"));
-        }
-    } else {
-        println!("Le mot de passe est incorrect!");
+    loop {
+        let mut message = String::new();
+        io::stdin().read_line(&mut message)?;
+        write!(stream, "{}", message)?;
+        let mut buffer = [0; 1024];
+        reader.read(&mut buffer)?;
+        print!("{}", str::from_utf8(&buffer).expect("Invalid UTF-8"));
     }
     Ok(())
 }
--- src/server.rs
+++ src/server.rs
@@ -1,63 +1,55 @@
-use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::io::{Read, Write};
 
 fn handle_client(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
 
-    // Attends que le client envoie le mot de passe
-    stream.read(&mut buffer).unwrap();
-    let password = String::from_utf8_lossy(&buffer[..]).trim().to_string();
-
-    // Vérifie le mot de passe
-    if password != "my_secret_password" {
-        stream.write(b"Mot de passe incorrect\n").unwrap();
-        return;
-    }
-
-    // Demande le nom d'utilisateur
-    stream.write(b"Entrez votre nom d'utilisateur: ").unwrap();
-
-    // Attends que le client envoie le nom d'utilisateur
-    buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
-    let username = String::from_utf8_lossy(&buffer[..]).trim().to_string();
-
-    // Affiche un message pour indiquer que le client a rejoint le chat
-    println!("{} a rejoint le chat", username);
-
-    // Boucle pour recevoir les messages du client
     loop {
-        buffer = [0; 1024];
-        match stream.read(&mut buffer) {
-            Ok(0) => break, // Le client a fermé la connexion
-            Ok(n) => {
-                let message = String::from_utf8_lossy(&buffer[..n]).trim().to_string();
-                println!("{}: {}", username, message);
+        let bytes_read = match stream.read(&mut buffer) {
+            Ok(0) => return,
+            Ok(bytes_read) => bytes_read,
+            Err(_) => {
+                eprintln!("An error occurred while reading from stream");
+                return;
             }
-            Err(e) => {
-                eprintln!("Erreur de lecture: {}", e);
-                break;
+        };
+
+        let message = match String::from_utf8(Vec::from(&buffer[..bytes_read])) {
+            Ok(message) => message,
+            Err(_) => {
+                eprintln!("Received invalid UTF-8 from stream");
+                return;
+            }
+        };
+
+        println!("Received message: {}", message.trim());
+
+        match stream.write(message.as_bytes()) {
+            Ok(_) => {},
+            Err(_) => {
+                eprintln!("An error occurred while writing to stream");
+                return;
             }
         }
     }
-
-    // Ferme la connexion
-    println!("{} a quitté le chat", username);
 }
 
-fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:5000")?;
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:5000").unwrap();
 
-    // Accepte les connexions en boucle
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
                 println!("Connexion acceptée de {}", stream.peer_addr().unwrap());
-                std::thread::spawn(|| handle_client(stream));
+
+                thread::spawn(move || {
+                    handle_client(stream)
+                });
             }
-            Err(e) => eprintln!("Erreur de connexion: {}", e),
+            Err(e) => {
+                eprintln!("Une erreur s'est produite lors de la réception d'une connexion : {}", e);
+            }
         }
     }
-
-    Ok(())
 }
