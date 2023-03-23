5a98dbb5af504f59490dca1a8293610f5215cdfb
DALM1 <92697916+DALM1@users.noreply.github.com>
2023-03-23 22:10:30 +0100

    Updates

--- src/server/server.py
+++ src/server/server.py
@@ -6,39 +6,37 @@ PORT = 5000  # Port à utiliser
 # Mot de passe pour se connecter au serveur
 PASSWORD = 'my_secret_password'
 
-# Nom d'utilisateur connecté
+
 USERNAME = None
 
-# Création d'une socket TCP/IP
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Association de la socket à l'adresse IP et au port
+
 sock.bind((HOST, PORT))
 
-# Mise en écoute de la socket
 sock.listen()
 
 print(f"Le serveur écoute sur {HOST}:{PORT}")
 
 while True:
-    # Attente d'une connexion
+   
     print("En attente de connexion...")
     conn, addr = sock.accept()
     print(f"Connexion acceptée de {addr[0]}:{addr[1]}")
 
-    # Lecture du mot de passe envoyé par le client
+   
     password = conn.recv(1024).decode().strip()
 
-    # Vérification du mot de passe
+    
     if password != PASSWORD:
         conn.send("Mot de passe incorrect".encode())
         conn.close()
         continue
 
-    # Envoi d'un message de bienvenue
+   
     conn.send("Bienvenue au serveur !".encode())
 
-    # Lecture du nom d'utilisateur envoyé par le client
+   
     username = conn.recv(1024).decode().strip()
     USERNAME = username
 
